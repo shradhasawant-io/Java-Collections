0492b9b5ea6b0fe3f7ff884fab91cf89dac7f026
Shradha Sawant <shraddha888sawant@gmail.com>
2026-09-21 22:14:41 +0530

    ArrayList Practice

---
+++ src/main/java/com/example/ArrayListImpl.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import java.util.List;
+
+public class ArrayListImpl {
+    public static void main(String[] args) {
+        List<Integer> list = new java.util.ArrayList<>();
+        list.add(1);
+        list.add(20);
+        list.add(83);
+        list.add(1);
+        list.add(1);
+
+        System.out.println(list);
+        System.out.println(list.get(3));
+        System.out.println(list.size());
+        for(int i = 0; i<list.size(); i++){
+            System.out.println(list.get(i));
+        }
+
+        for(int i: list){
+            System.out.println(i);
+        }
+
+        System.out.println(list.remove(4));
+        System.out.println(list);
+    }
+}
