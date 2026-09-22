706fb8268c1730c4cbea1b6d3682b9e10bfcef22
Shradha Sawant <shraddha888sawant@gmail.com>
2026-09-22 12:28:40 +0530

    ArrayList Practice

--- src/main/java/com/example/ArrayListI.java
+++ src/main/java/com/example/ArrayListI.java
@@ -2,7 +2,7 @@ package com.example;
 
 import java.util.List;
 
-public class ArrayListImpl {
+public class ArrayListI {
     public static void main(String[] args) {
         List<Integer> list = new java.util.ArrayList<>();
         list.add(1);
