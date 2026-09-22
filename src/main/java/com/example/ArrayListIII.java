35e4aecbae887607ef9a0f013558d416046d32e8
Shradha Sawant <shraddha888sawant@gmail.com>
2026-09-22 12:36:50 +0530

    ArrayList Practice

--- src/main/java/com/example/ArrayListIII.java
+++ src/main/java/com/example/ArrayListIII.java
@@ -1,4 +1,21 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ArrayListIII {
+    public static void main(String[] args) {
+        List<Integer> l = new ArrayList<>();
+        l.add(1);
+        l.add(2);
+        l.add(3);
+        l.add(4);
+        //System.out.println(l);
+
+        List<Integer> list = Arrays.asList(5, 6, 7, 8, 9, 10);
+        l.addAll(list);
+        System.out.println(l);
+    }
+
 }
