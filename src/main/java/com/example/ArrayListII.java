c336fd524f6c8a3de7f82f3a883e37a32788e95a
Shradha Sawant <shraddha888sawant@gmail.com>
2026-09-22 12:26:35 +0530

    ArrayList Practice

--- src/main/java/com/example/ArrayListII.java
+++ src/main/java/com/example/ArrayListII.java
@@ -13,5 +13,13 @@ public class ArrayListII {
         System.out.println(list1.getClass().getName());
         list1.set(2,"d");
         System.out.println(list1);
+
+        List<Integer> list2 = List.of(1, 2, 3);
+        System.out.println(list2.getClass().getName());
+        System.out.println(list2);
+
+        String[] array = {"a", "b", "c"};
+        List<String> list3 = Arrays.asList(array);
+        System.out.println(list3.getClass().getName());
     }
 }
